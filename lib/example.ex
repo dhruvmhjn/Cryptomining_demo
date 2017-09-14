d26d514e2683fc5ac398e46386013717f3892786
blackthorn39 <blackthorn_39@live.in>
2017-09-14 12:51:14 -0400

    escript command line

---
+++ lib/example.ex
@@ -0,0 +1,30 @@
+defmodule Example do
+  @moduledoc """
+  Documentation for Example.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Example.hello
+      :world
+
+  """
+  def main(args) do
+    args |> parse_args |> process
+  end
+
+  def process({[],[],[]}) do
+    IO.puts "No arguments given"
+  end
+
+  def process({_, [options], _}) do
+    IO.puts "You want #{options} leading zeroes"
+  end
+  defp parse_args(args) do
+     cmdarg = OptionParser.parse(args)
+     cmdarg
+  end
+ end
