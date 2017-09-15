ce123d8053b51425d33676e1367204952858800b
blackthorn39 <blackthorn_39@live.in>
2017-09-15 17:13:42 -0400

    tail recursion

--- lib/example.ex
+++ lib/example.ex
@@ -1,13 +1,5 @@
 defmodule Example do
-  # @moduledoc """
-  # Documentation for Example.
-  # """
-  # @doc """
-  # Hello world.
-  # ## Examples
-  #     iex> Example.hello
-  #     :world
-  # """
+
   def main(args) do
     args |> parse_args |> process
   end
@@ -16,34 +8,20 @@ defmodule Example do
     IO.puts "No arguments given"
   end
 
-  def process({_, [k_value], _}) do
-    #Myspawn.createprocess
-    
+  def process({[], [k_value], []}) do
+        
     x = [k_value]
-    IO.puts x
-    
-    IO.puts "You want #{k_value} leading zeroes"
     abc = :rand.uniform(40)
-    IO.puts "The length of the random string is: "<>to_string(abc+10)
     rand_string = random_string(abc+10)
     input_srt= "dhruvmhjn"<>rand_string
-    IO.puts input_srt
     hashed_srt = Base.encode16(:crypto.hash(:sha256, input_srt))
-    IO.puts "SHA256, base 16 hash is: "<>hashed_srt
     regex = ~r/^0{#{k_value}}/
-    checkvalid(Regex.match?(regex,hashed_srt),input_srt,hashed_srt)
-    #process(,x,) MAKE THIS TAIL RECURSIVE, CANT CALL PROCESS 
-    process({_, x, _})
+    if Regex.match?(regex,hashed_srt) do
+      IO.puts input_srt<>"\t"<>hashed_srt
+    end
+    
+    process({[], x, []})
   end
-
-  def checkvalid(true,input,output) do
-    IO.puts "VALID Bitcoin for string: #{input}"
-  end
-
-  def checkvalid(false,_,_) do
-    IO.puts "Invalid Bitcoin"
-  end
-
 
   defp random_string(length) do
     Base.encode64(:crypto.strong_rand_bytes(length))
