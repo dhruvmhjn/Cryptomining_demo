26706c94dc7284984c169448907ad9dafdf916ed
dhruvmhjn <dhruvmhjn@gmail.com>
2017-09-15 14:00:52 -0400

    Code Cleanup

--- lib/example.ex
+++ lib/example.ex
@@ -1,17 +1,13 @@
 defmodule Example do
-  @moduledoc """
-  Documentation for Example.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Example.hello
-      :world
-
-  """
+  # @moduledoc """
+  # Documentation for Example.
+  # """
+  # @doc """
+  # Hello world.
+  # ## Examples
+  #     iex> Example.hello
+  #     :world
+  # """
   def main(args) do
     args |> parse_args |> process
   end
@@ -22,15 +18,16 @@ defmodule Example do
 
   def process({_, [options], _}) do
     #Myspawn.createprocess
+    
     x = [options]
     IO.puts x
+    
     IO.puts "You want #{options} leading zeroes"
-    IO.puts "Now lets try to compute a random string hash"
     abc = :rand.uniform(40)
-    IO.puts "The length of the random string is "<>to_string(abc+10)
+    IO.puts "The length of the random string is: "<>to_string(abc+10)
     rand_string = random_string(abc+10)
     IO.puts "dhruvmhjn"<>rand_string
-    IO.puts Base.encode16(:crypto.hash(:sha256, "dhruvmhjn"<>rand_string))
+    IO.puts "SHA256, base 16 hash is: "<>Base.encode16(:crypto.hash(:sha256, "dhruvmhjn"<>rand_string))
     #process(,x,) MAKE THIS TAIL RECURSIVE, CANT CALL PROCESS 
   end
 
