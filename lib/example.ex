7ee5327f7272a80a4419e857334616b243289440
blackthorn39 <blackthorn_39@live.in>
2017-09-15 16:26:18 -0400

    regex

--- lib/example.ex
+++ lib/example.ex
@@ -16,20 +16,34 @@ defmodule Example do
     IO.puts "No arguments given"
   end
 
-  def process({_, [options], _}) do
+  def process({_, [k_value], _}) do
     #Myspawn.createprocess
     
-    x = [options]
+    x = [k_value]
     IO.puts x
     
-    IO.puts "You want #{options} leading zeroes"
+    IO.puts "You want #{k_value} leading zeroes"
     abc = :rand.uniform(40)
     IO.puts "The length of the random string is: "<>to_string(abc+10)
     rand_string = random_string(abc+10)
-    IO.puts "dhruvmhjn"<>rand_string
-    IO.puts "SHA256, base 16 hash is: "<>Base.encode16(:crypto.hash(:sha256, "dhruvmhjn"<>rand_string))
+    input_srt= "dhruvmhjn"<>rand_string
+    IO.puts input_srt
+    hashed_srt = Base.encode16(:crypto.hash(:sha256, input_srt))
+    IO.puts "SHA256, base 16 hash is: "<>hashed_srt
+    regex = ~r/^0{#{k_value}}/
+    checkvalid(Regex.match?(regex,hashed_srt),input_srt)
     #process(,x,) MAKE THIS TAIL RECURSIVE, CANT CALL PROCESS 
+    #process({_, [k_value], _})
   end
+
+  def checkvalid(true,input) do
+    IO.puts "VALID Bitcoin for string: #{input}"
+  end
+
+  def checkvalid(false,_) do
+    IO.puts "Invalid Bitcoin"
+  end
+
 
   defp random_string(length) do
     Base.encode64(:crypto.strong_rand_bytes(length))
