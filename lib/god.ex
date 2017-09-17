b2ad2324e36712ae33b4fa9dbbb00712891f3a48
blackthorn39 <blackthorn_39@live.in>
2017-09-16 23:36:30 -0400

    command args differentiation

--- lib/god.ex
+++ lib/god.ex
@@ -1,10 +1,23 @@
 defmodule God do
     def main(args) do
-        args |> parse_args |> ServMinerSup.init()
+        args |> parse_args #|> ServMinerSup.init()
     end
 
     defp parse_args(args) do
-        cmdarg = OptionParser.parse(args)  
-        cmdarg
+        cmdarg = OptionParser.parse(args) 
+        {[],[argumentstr],[]} = cmdarg
+        kregex = ~r/^\d{1,2}$/
+        ipregex = ~r/^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/
+
+        if Regex.match?(ipregex,argumentstr)do
+            IO.puts "Matched IP value"
+        else if Regex.match?(kregex,argumentstr) do
+            IO.puts "Matched K value"
+            
+        else
+            IO.puts "Invalid input"
+        end 
+        end
+        #cmdarg
     end
 end
