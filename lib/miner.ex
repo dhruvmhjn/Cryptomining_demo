c2af917288f01c8ce0e77200040b26b08dbb2968
blackthorn39 <blackthorn_39@live.in>
2017-09-16 22:17:52 -0400

    regex

--- lib/miner.ex
+++ lib/miner.ex
@@ -3,8 +3,8 @@ defmodule Miner do
     length = :rand.uniform(10) 
     input_srt= "dhruvmhjn"<>Integer.to_string(x)<>Base.encode64(:crypto.strong_rand_bytes(length))
     hashed_srt = Base.encode16(:crypto.hash(:sha256, input_srt))
-    regex = ~r/^0{#{k_value}}/
-    if Regex.match?(regex,hashed_srt) do
+    coin_regex = ~r/^0{#{k_value}}/
+    if Regex.match?(coin_regex,hashed_srt) do
       IO.puts input_srt<>"\t"<>hashed_srt
     end
     process(k_value,x)
